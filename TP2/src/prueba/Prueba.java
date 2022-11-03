1399b9d4b5d286bb3425f2688088e095600f9b02
Silverado42 <43814714+Silverado42@users.noreply.github.com>
2022-11-03 12:03:05 -0300

    Principio TP2 Huffman

---
+++ TP2/src/prueba/Prueba.java
@@ -0,0 +1,16 @@
+package prueba;
+
+import modelo.PrimeraParte;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PrimeraParte primeraparte = new PrimeraParte();
+		
+		primeraparte.leeArchivo();
+		primeraparte.procesamiento();
+		primeraparte.huffman();
+	}
+
+}
